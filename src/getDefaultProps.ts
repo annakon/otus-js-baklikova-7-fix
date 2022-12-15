5948005558f89d97051c238a34b3e786ef6bacf0
denis <denis.baklykov@boxberry.ru>
2022-12-15 15:19:34 +0300

    getDefaultProps

--- src/getDefaultProps.ts
+++ src/getDefaultProps.ts
@@ -2,11 +2,12 @@
 // Нужно заменить FIXME на правильный тип
 import React from "react"
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type FIXME = any;
+type FIXME<T> = Partial<T> | undefined;
 
 // Hint: infer
 export const getDefaultProps = <T>(
     component: React.ComponentType<T>
-): GetUserOrderStates => {
+): FIXME<T>  => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-return
     return component.defaultProps;
 };
